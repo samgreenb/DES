229c8813d914fb07a93a5af3ea1123a4b7ec5775
samgreen <2300389@abertay.ac.uk>
2024-02-29 19:08:54 +0000

    melt audio control

--- Assets/Scripts/Ice Statues/DissapearEffect.cs
+++ Assets/Scripts/Ice Statues/DissapearEffect.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class DissapearEffect : IceStatueEffect
 {
+    [SerializeField]
+    private AK.Wwise.Event meltSound = new AK.Wwise.Event();
+
     public override void Activate()
     {
-        Destroy(gameObject);
+        Debug.Log("playmeltsound");
+        meltSound.Post(gameObject);
+        Invoke(nameof(Hide), 1.0f);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
     }
 }
