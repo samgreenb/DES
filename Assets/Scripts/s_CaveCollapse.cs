2b1e2704574559e7fc6b10c490f3e9d2dfaec505
ConnisourMan <dw80106@gmail.com>
2024-03-07 18:33:32 +0000

    More stages greyboxed
    Cave collapsing to the abyss have been greyboxed

---
+++ Assets/Scripts/s_CaveCollapse.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s_CaveCollapse : MonoBehaviour
+{
+    public GameObject HiddenWall;
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        HiddenWall.SetActive(true);
+    }
+}
