a80dbdf498fcb62ab34d34e5c5f8460616ec7959
ConnisourMan <dw80106@gmail.com>
2024-03-21 17:37:15 +0000

    Update
    Added jeff's current particles
    Added some more rocks to the scene

---
+++ Assets/Prefabs/Particle FXs/README.cs
@@ -0,0 +1,43 @@
+///De Wick Particle Effect rundown
+/// 
+/// Parameters toggled      
+///                                 Snow            Dust(Enviro)        Shockwave        
+/// Duration                        5                   5          
+/// Prewarm                         o                   x
+/// Start Speed                     0                   0.2
+/// Start Size                  0.05 - 0.1              *0.1
+/// Start Lifetime                  8                   ???
+/// Start Colour                E0DBDB - FFFFFF         ???
+/// 
+/// Emmision/Rate Over Time         100                 *100
+/// 
+/// Shape/Shape                     Box                 Sphere
+/// Shape/Scale                     15/1/15
+/// Shape/Radius                    n/a                 adjustable
+///                         
+/// VoL/Linear                  -2=2/-2=-4/-2=2         n/a
+/// 
+/// SizeOverLife/Size                               0,0;0.5,1.0;1,0              
+/// 
+/// ColourOverLife           Alpha 255-255-0            ???     
+/// 
+/// Collision/Type                  World               n/a
+/// Collision/Bounce                0                   n/a
+/// Collision/Dampen                1                   n/a
+/// Collision/RadiusScale           0.4                 n/a
+/// Col/ColQual/CollidesWith    Play w/ layers          n/a
+///                             To improve perf         
+/// 
+/// Renderer/material               SnowPFx             ??
+/// Rend/maxParticleSize            0.005               ??
+/// 
+/// ====================================================================
+/// 
+/// Values to change *
+/// 
+/// Snow            = Play around with size and velocity and speed/density of snow
+/// DustEnviro      = Play around with Shape Radius, Size and Speed
+/// 
+/// 
+/// 
+
